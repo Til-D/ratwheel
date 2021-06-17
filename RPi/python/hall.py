34ea52636a340c04d92ce0f141f8babac0a90fb6
tilman <til-d@gmx.de>
2021-06-17 13:53:48 +1000

    hall sensor: rpms and timer

--- RPi/python/hall.py
+++ RPi/python/hall.py
@@ -1,38 +1,128 @@
 #!/usr/bin/python
 #--------------------------------------
-#    ___  ___  _ ____
-#   / _ \/ _ \(_) __/__  __ __
-#  / , _/ ___/ /\ \/ _ \/ // /
-# /_/|_/_/  /_/___/ .__/\_, /
-#                /_/   /___/
 #
 #       Odometer
 #
-# This script tests the sensor on GPIO17.
+# This script uses a hall sensor on GPIO17.
 #
-# Author : Matt Hawkins
-# Date   : 08/05/2018
+# Author : Tilman Dingler
+# Date   : 17/06/2021
 #
-# https://www.raspberrypi-spy.co.uk/
+# https://www.github.com/Til-D/ratwheel.git
 #
 #--------------------------------------
 
-# Import required libraries
 import time
 import datetime
 import RPi.GPIO as GPIO
+import os
+from functools import reduce
+import threading
+
+from server_connection import *
+from TimerReset import *
+
+# returns current timestamp in ms
+def now():
+    return round(time.time()*1000)
+
+def trigger_timeout():
+  print("## TIMEOUT ##")
+
+  global timedout
+  global session_id
+  global rotations
+  global rpms
+  global timer
+
+  if(not timedout): # notify server that wheel has stopped
+      post_rotations(SERVER_URL, DEVICE_ID, [0], session_id, 0, now())
+  
+  # init new session
+  session_id = "new"
+  rotations = 0
+  rpms = []
+  timedout = True
+  timer = None
+
+# Globals
+GRANULARITY = 2 #determines how many rotations determine rpm
+SERVER_URL = SERVER_PROD
+DEVICE_ID = os.environ['DEVICE_ID']
+TIMEOUT = os.environ['SESSION_TIMEOUT'] #sec
+
+ts_last = 0
+pole_last = 1 #HIGH
+session_id = "new"
+rotations = 0
+rpms = []
+timedout = False
+timer = None
 
 def sensorCallback(channel):
   # Called if sensor output changes
-  timestamp = time.time()
-  stamp = datetime.datetime.fromtimestamp(timestamp).strftime('%H:%M:%S')
-  if GPIO.input(channel):
+  timestamp = now()
+  stamp = datetime.datetime.fromtimestamp(time.time()).strftime('%H:%M:%S')
+  pole = GPIO.input(channel)
+  global ts_last
+  global pole_last
+  global session_id
+  global rotations
+  global rpms
+  global timedout
+  global timer
+  
+  if pole:
     # No magnet
-    print("Sensor HIGH " + stamp)
+    # print("Sensor HIGH " + stamp)
+    pass
+    
   else:
     # Magnet
-    print("Sensor LOW " + stamp)
+    # print("Sensor LOW " + stamp)
+    
+    # calculate rpm
+    diff = timestamp - ts_last #ms
+    
+    if(ts_last > 0 and diff > 0):
+        
+      timeout = TIMEOUT * 1000 #ms
+      if(diff < timeout):
 
+        if(timer is not None):
+            timer.reset()
+        else:
+            if(timer):
+              timer.start()
+            else:
+              timer = TimerReset(TIMEOUT, trigger_timeout)
+              timer.start()
+              
+        timedout = False
+    
+        diff = diff/(60*1000) #min
+        rpm = round(1/diff, 2)
+ 
+        rotations += 1
+        rpms.append(rpm)
+      
+        if(rotations == GRANULARITY):
+          
+          avg_rpm = round(reduce(lambda a, b: a+b, rpms) / len(rpms), 2)
+        
+          print("rpm: " + str(avg_rpm) + ", rotations: " + str(rotations)) 
+          
+          # send rpm to server and rempty rotations
+          rotations = 0
+          rpms = []
+          session_id = post_rotations(SERVER_URL, DEVICE_ID, avg_rpm, session_id, rotations, timestamp)
+          
+      else: #timeout
+          pass
+          
+    pole_last = pole
+    ts_last = timestamp
+  
 def main():
   # Wrap main content in a try block so we can
   # catch the user pressing CTRL-C and run the
@@ -55,7 +145,9 @@ def main():
 # Tell GPIO library to use GPIO references
 GPIO.setmode(GPIO.BCM)
 
+print("Device id: " + DEVICE_ID)
 print("Setup GPIO pin as input on GPIO17")
+ping_server(SERVER_URL, DEVICE_ID)
 
 # Set Switch GPIO as input
 # Pull high by default
--- RPi/python/server_connection.py
+++ RPi/python/server_connection.py
@@ -10,39 +10,43 @@
 import time
 import datetime
 import requests
+import os
+import json
 
 SERVER_PROD = 'http://45.113.235.98'
 SERVER_TEST = 'http://localhost:3000'
 
-DEVICE_ID = 'ratwheel_test'
 
 def main():
 
-	ping_server(SERVER_PROD, DEVICE_ID)
-	post_rotations(SERVER_PROD, DEVICE_ID, 54, "test1337", 120, int(time.time()))
+    ping_server(SERVER_PROD, os.environ['DEVICE_ID'])
+    post_rotations(SERVER_PROD, os.environ['DEVICE_ID'], 54, "new", 120, int(time.time()))
 
 def ping_server(server_url, device_id):
-	ping_url = server_url + '/api/ping'
-	ping = requests.post(ping_url, json={"deviceId": device_id})
-	print('- ping sent to server: ' + ping_url)
-	print('+ status code: ' + str(ping.status_code))
-	print('+ repsonse: ' + ping.text)
+    ping_url = server_url + '/api/ping'
+    ping = requests.post(ping_url, json={"deviceId": device_id})
+    print('- ping sent to server: ' + ping_url)
+    print('+ status code: ' + str(ping.status_code))
+    print('+ repsonse: ' + ping.text)
 
 def post_rotations(server_url, device_id, rpm, session_id, rotations, ts):
-	post_url = server_url + '/api/rpm'
-	obj = {
-		"deviceId": device_id,
-		"rpm": rpm,
-		"sessionId": session_id,
-		"rotations": rotations,
-		"ts": ts
-	}
-	r = requests.post(post_url, json=obj)
-	print('- rpm sent to server: ' + post_url)
+    post_url = server_url + '/api/rpm'
+    obj = {
+        "deviceId": device_id,
+        "rpm": rpm,
+        "sessionId": session_id,
+        "rotations": rotations,
+        "ts": ts
+    }
+    r = requests.post(post_url, json=obj)
+    print('- rpm sent to server: ' + post_url)
 
-
-	print('+ status code: ' + str(r.status_code))
-	print('+ repsonse: ' + r.text)
+    # print('+ status code: ' + str(r.status_code))
+    # print('+ repsonse: ' + r.text)
+    
+    if(r.status_code==200):
+        session = json.loads(r.text)
+        return session.get('sessionId')
 
 if __name__=="__main__":
    main()
